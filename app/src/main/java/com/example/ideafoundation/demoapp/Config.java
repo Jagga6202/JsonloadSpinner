38efb12a2215650da722997ea6f5796143812232
Jagdeep Singh <jagdeeps@ideafoundation.in>
2017-01-10 21:13:00 +0530

    first commit

---
+++ app/src/main/java/com/example/ideafoundation/demoapp/Config.java
@@ -0,0 +1,20 @@
+package com.example.ideafoundation.demoapp;
+
+/**
+ * Created by ideafoundation on 10/01/17.
+ */
+
+public class Config {
+    //JSON URL
+    public static final String DATA_URL = "http://www.androidbegin.com/tutorial/jsonparsetutorial.txt";
+
+    //Tags used in the JSON String
+    public static final String TAG_RANK = "rank";
+    public static final String TAG_POPULATION = "population";
+    public static final String TAG_FLAG = "flag";
+    public static final String TAG_COUNTRY = "country";
+
+
+    //JSON array name
+    public static final String JSON_ARRAY = "worldpopulation";
+}
